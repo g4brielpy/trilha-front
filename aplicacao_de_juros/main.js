9535724f3b4ce8ee0f402c2016820e98a1539151
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-03-18 20:47:45 -0300

    Instalação do readline

---
+++ aplicacao_de_juros/main.js
@@ -0,0 +1,10 @@
+// Valor da dívida: user informar
+// Tempo de vencimento do boleto: user informar
+// Taxa de juros: 10% p/ mais de 15 dias de atraso, caso contrário, 5%
+
+// -- APLICAÇÃO DE JUROS --
+let taxaJuros;
+
+console.log('APLICAÇÃO DE JUROS 5')
+
+
