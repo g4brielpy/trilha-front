d17a8987e24760dde1253c3adcadf32a6f0f56cd
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-06-19 09:00:25 -0300

    Estilização da seção banner

--- react/my-app/src/components/section-banner.jsx
+++ react/my-app/src/components/section-banner.jsx
@@ -1,6 +1,6 @@
 export function SectionBanner() {
   return (
-    <section>
+    <section id="section-banner">
       <h1 className="titulo-banner">React JS</h1>
       <h2 className="sub-titulo-banner">Biblioteca JavaScript para criar SPAs</h2>
       <a className="button-react" href="https://react.dev/" target="_blank">Site Oficial</a>
