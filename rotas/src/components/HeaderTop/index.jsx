aa73e495eff9bea36c9ea4128377d8ce87511405
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-07-08 09:34:13 -0300

    Component HeaderTop

--- rotas/src/components/HeaderTop/index.jsx
+++ rotas/src/components/HeaderTop/index.jsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
+import styles from "./index.module.css";
 
 export default function HeaderTop() {
   return (
-    <header>
-      <nav>
-        <p>sdadasd</p>
+    <header className={styles.header}>
+      <nav className={styles.containerLinks}>
         <Link href={"/frontend"}>Front-End</Link>
         <Link href={"/backend"}>Back-End</Link>
         <Link href={"/mobile"}>Mobile</Link>
