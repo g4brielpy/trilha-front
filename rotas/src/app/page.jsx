1dd523c0b9c774251570deda277993dd19a0786b
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-07-08 09:29:16 -0300

    Página Inicial

--- rotas/src/app/page.jsx
+++ rotas/src/app/page.jsx
@@ -1,5 +1,11 @@
-import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
-  return (<h1>Teste</h1>  );
+  return (
+    <main className={styles.main}>
+      <div>
+        <h1>Seja bem-vindo ao exemplo de navegação no React</h1>
+      </div>
+    </main>
+  );
 }
