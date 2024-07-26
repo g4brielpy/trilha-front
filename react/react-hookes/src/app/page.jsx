70d23c8bab7448ca2c73de9dc5ff3d35ef7d0b65
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-07-26 10:34:21 -0300

    Usando useState

---
+++ react/react-hookes/src/app/page.jsx
@@ -0,0 +1,36 @@
+"use client";
+import { useState } from "react";
+
+export default function Home() {
+  const [contador, setContador] = useState(0);
+  const [tranca, setTranca] = useState(true);
+
+  return (
+    <>
+      <h1 className="text-blue-950 text-4xl text-center font-bold my-12">
+        Hooks no React/Next
+      </h1>
+      <div className="mb-16 px-8 py-2">
+        <h2 className="mb-4">useState</h2>
+        <p className="text-xl mb-2">Voce clicou {contador} no botão</p>
+        <button
+          className="py-2 px-6 rounded-lg bg-blue-950 text-white"
+          onClick={() => setContador(contador + 1)}
+        >
+          Click Aqui!
+        </button>
+      </div>
+
+      <div className="mb-16 px-8 py-2">
+        <h2 className="mb-4">useState</h2>
+        <p className="text-xl mb-2">Valores booleanos</p>
+        <button
+          className="py-2 px-6 rounded-lg bg-blue-950 text-white"
+          onClick={() => setTranca(!tranca)}
+        >
+          {tranca ? "Fechado" : "Aberto"}
+        </button>
+      </div>
+    </>
+  );
+}
